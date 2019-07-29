09397ff2131464eb328cc88304d7bfc99edb4619
jennahamlin <jennahamlin@gmail.com>
2019-07-29 14:47:41 -0400

    fixed fig 3

--- scripts/Code.For.Figures2.3.and4.R
+++ scripts/Code.For.Figures2.3.and4.R
@@ -4,18 +4,12 @@ library(grid)
 library(gridBase)
 tiff("Fig.2.tiff", units="in", width=11, height=8.5, res=300)
 
-#setwd("~/Desktop/Figures.Final")
 #####Figure 2 - Plot of all Genome-wide D values with mean and 95% confidence intervals for geographic trios
 
 #####################geography trios and entire genomes
 mydata<-read.csv('solanum.all.combined.windows.csv', header =T)
 #only keep rows in which Dsites is greater than 20
 mydata<- mydata[which(mydata$Dsites > 20),]
-#calculate the adjusted pvalue via bonferroniicorrection
-#adjustedp<-p.adjust(mydata$Pvalue, "bonferroni", n = length(mydata$Pvalue))
-#append this numeric vector to the end of mydata
-#mydata<-cbind(mydata, adjustedp)
-#mydata<-subset(mydata, adjustedp<0.06, select=c(group, Dstat.alwaysABBAminusBABA))
 
 mydata$group<-factor(mydata$group, levels=c('gal.gal.che',"arc.arc.pim", "pim.pim.neo", 'pim.pim.chi', "pim.pim.cor1", "pim.pim.cor2",
                                             "pim.pim.per1", "pim.pim.per2", "pim.pim.hab", 'pim.pim.pen',"arc.arc.hab",
@@ -26,12 +20,11 @@ my.labels<-c('gal\n\ngal\n\nche', 'arc\n\narc\n\npim', 'pim\n\npim\n\nneo', 'pim
              'pim\n\npim\n\nhab', 'pim\n\npim\n\npen', 'arc\n\narc\n\nhab', 'hab\n\nhab\n\nneo',
              'hab\n\nhab\n\ncor',  'hua\n\nhua\n\nhab', 'per\n\nper\n\npen2') 
  
-# Create a data frame 'estimates' to hold...
-# the means and 95% confidence intervals:
+# Create a data frame 'estimates' to hold the means and 95% confidence intervals:
 estimates <- expand.grid(group=levels(mydata$group))
 #  mean D using tapply()
 estimates$MeanD  <- tapply(mydata$Dstat.alwaysABBAminusBABA, mydata$group, mean)
-##### Means with 95% CIs
+##### Means with 95% CIs using a linear model 
 lm1<-lm(Dstat.alwaysABBAminusBABA ~ group, data=mydata)
 # add 95% CI upper and lower bounds to estimates dataframe
 estimates$C95_lwr  <- predict(lm1, newdata= estimates, interval="confidence")[,2]
@@ -40,10 +33,6 @@ estimates$C95_upr  <- predict(lm1, newdata= estimates, interval="confidence")[,3
 estimates$sum.BBAA<-tapply(mydata$BBAA, mydata$group, sum)
 estimates$sum.ABBA<-tapply(mydata$ABBA, mydata$group, sum)
 estimates$sum.BABA<-tapply(mydata$BABA, mydata$group, sum)
-
-
-#subset and just plot the values which are significant for first visulization
-#mydata<-subset(mydata, adjustedp<0.06, select=c(group, Dstat.alwaysABBAminusBABA))
 
 ggplot()+
   geom_point(data=mydata, alpha = 1/3, color= ifelse(mydata$Pvalue<0.06, 'deepskyblue', 'lightgray'), 
@@ -64,6 +53,7 @@ ggplot()+
   theme(plot.margin = unit(c(1, 9, 4, 1), "lines")) #extra space on the right 
 
 
+##this is to add text to the right of the plot which shows the difference between +D and and -D with a bracket
 grid.text(x = unit(0.93, "npc"), y = unit(0.725, "npc") ,label="+D\nGreater\n introgression\n between\n proximate pairs ")
 grid.text(x = unit(0.93, "npc"), y = unit(0.4, "npc") ,label="-D\n Greater\n introgression\n between\n distant pairs ")
 
@@ -76,6 +66,7 @@ grid.lines(x = unit(c(0.85,0.85 ), "npc"), y = unit(c(0.26,0.593), "npc"))#verti
 grid.lines(x = unit(c(0.85,0.84 ), "npc"), y = unit(c(0.26,0.26), "npc"))
 
 
+##this draws the phylogenetic tree with the words distant, proximate, and outgroup placed appropriately 
 grid.text(x = unit(0.87, "npc"), y = unit(0.21, "npc") ,label="Distant")
 grid.text(x = unit(0.87, "npc"), y = unit(0.16, "npc") ,label="Proximate")
 grid.text(x = unit(0.87, "npc"), y = unit(0.11, "npc") ,label="Proximate")
@@ -90,7 +81,6 @@ grid.lines(x = unit(c(0.91,.96), "npc"), y = unit(c(0.11, 0.17), "npc")) # p3
 grid.lines(x = unit(c(0.91,.99), "npc"), y = unit(c(0.06, 0.15), "npc")) # outgroup
 
 dev.off()
-
 
 #sympartic trios sliced independently in order to test if each trio mean is significantly different than zero
 #and this is for site that are greater than 20
@@ -159,12 +149,6 @@ pim.pim.per2<-mydata %>%
 
 arcA<-sum(arc.arc.hab$Dleft)
 arcB<-sum(arc.arc.hab$Dright)
-
-test <-c(111486, 117313)
-chisq.test(test, p = c(1/2, 1/2))
-
-
-
 arcchi<-((arcA-arcB)^2)/(arcA+arcB)
 arcchi
 pchisq(arcchi, df=1, lower.tail = F)
@@ -268,22 +252,14 @@ p.adjust(p, method = "bonferroni", n = length(p))
 
 #####Figure 3 - Plot of all Genome-wide D values with mean and 95% confidence intervals for mating system trios
 
+tiff("Fig.3.tiff", units="in", width=11, height=8.5, res=300)
+
 #####################mating system trios and entire genomes
-mydata<-read.csv('matingsystem.trios.withLA0400.nopen.csv', header = T)
+mydata<-read.csv('/Users/jhamlin/Desktop/solanum.mating.LA0373.combined.windows.csv', header = T)
 #only keep rows in which Dsites is greater than 20
 mydata<- mydata[which(mydata$Dsites > 20),]
-#calculate the adjusted pvalue via bonferroniicorrection
-#adjustedp<-p.adjust(mydata$Pvalue, "bonferroni")
-#append this numeric vector to the end of mydata
-#mydata<-cbind(mydata, adjustedp)
 
-mydata<-mydata[order(-adjustedp),]
-
-my.labels<-c('arc.SI\n\narc.SC\n\npim.SC','hab.SI\n\nhab.SC\n\npim.SC',
-             'per.SI\n\nper.SI\n\npim.SC')  
-
-# Create a data frame 'estimates' to hold...
-# the means and 95% confidence intervals:
+# Create a data frame 'estimates' to holdnthe means and 95% confidence intervals:
 estimates <- expand.grid(group=levels(mydata$group))
 #  mean D using tapply()
 estimates$MeanD  <- tapply(mydata$Dstat.alwaysABBAminusBABA, mydata$group, mean)
@@ -298,18 +274,15 @@ lm1<-lm(Dstat.alwaysABBAminusBABA ~ group, data=mydata)
 estimates$C95_lwr  <- predict(lm1, newdata= estimates, interval="confidence")[,2]
 estimates$C95_upr  <- predict(lm1, newdata= estimates, interval="confidence")[,3]
 
-
-#subset and just plot the values which are significant
-#mydata<-subset(mydata, adjustedp<0.06, select=c(group, Dstat.alwaysABBAminusBABA))
+my.labels<-c('arc.SI\n\narc.SC\n\npim.SC','hab.SI\n\nhab.SC\n\npim.SC',
+             'per.SI\n\nper.SI\n\npim.SC')  
 
 ggplot()+
-  geom_point(data=mydata, color= ifelse(mydata$Pvalue<0.06, 'deepskyblue', 'lightgray'), 
-             aes(x=group,y=Dstat.alwaysABBAminusBABA ), position = position_jitter(w = 0.35, h = 0.35))+
-  #geom_quasirandom(data=mydata, color= ifelse(adjustedp<0.05, 'deepskyblue', 'lightgray'), 
-  #              aes(x=group,y=Dstat.alwaysABBAminusBABA ), show.legend =F, cex=1,
-  #             position = position_jitter(w = 0.3, h = 0.3))+
-  labs(y="Genome-wide D-statistic (mean with 95% CI)")+
+  geom_point(data=mydata, alpha = 1/3, color= ifelse(mydata$Pvalue<0.06, 'deepskyblue', 'lightgray'), 
+             aes(x=group,y=Dstat.alwaysABBAminusBABA ), position = position_jitter(w = 0.4))+
   theme_bw()+
+  theme(axis.title.x = element_blank())+
+  labs(y="Genome-wide D-statistic (mean with 95% CI)")+
   theme(panel.border = element_blank(),panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), strip.text.x = element_blank(),
         panel.spacing.x = unit(0,"line"))+
@@ -321,16 +294,21 @@ ggplot()+
   geom_errorbar(data=estimates, aes(x=group, ymin=MeanD- abs(C95_lwr), ymax=MeanD+ abs(C95_upr)), width=.1)+
   geom_hline(yintercept=c(0), linetype='dotted')
 
+dev.off()
+
 #mating systems trios sliced independently in order to test if each trio mean is significantly different than zero
 ##adjusted based on removing rows with less than 20 Dsites.
-arcSI.arcSC.pimSC<-slice(rawdat,1:5234) #1
-habSI.habSC.pimSC<-slice(rawdat,5235:11034 )#2 
-perSI.perSC.pimSC<-slice(rawdat,16022:20204)#4
+arcSI.arcSC.pimSC<-mydata %>%
+  group_by(group) %>%
+  filter(group == "arcSI.arcSC.pimSC")
 
-#all data
-arcSI.arcSC.pimSC<-slice(rawdat,1:8027) #1
-habSI.habSC.pimSC<-slice(rawdat,8028:16054 )#2 
-perSI.perSC.pimSC<-slice(rawdat,24082:32108)#4 
+habSI.habSC.pimSC<-mydata %>%
+  group_by(group) %>%
+  filter(group == "habSI.habSC.pimSC")
+
+perSI.perSC.pimSC<-mydata %>%
+  group_by(group) %>%
+  filter(group == "perSI.perSC.pimSC")
 
 arcA<-sum(arcSI.arcSC.pimSC$Dleft)
 arcB<-sum(arcSI.arcSC.pimSC$Dright)
@@ -353,10 +331,8 @@ pchisq(perchi, df=1, lower.tail = F)
 #padjust for only >greater than 20 sites
 p<-c(4.35E-162, 0, 0.00302)
 
-#padjust for all sites 
-p<-c(0,6.34e-55,0)
-
 p.adjust(p, method = "bonferroni", n = length(p))
+
 #####Figure 4 - Absolute value of Genome-wide D statistic regressed againist Genetic Distance for all 17 comparisons
 
 gen.dis<-c(0.34, 2.95, 2.95, 3.02, 
@@ -373,7 +349,6 @@ mean.d<-abs(c(-0.027, 0.032, -0.089, -0.010,
 
 geoplot<-plot(x=gen.dis, y=mean.d, xlab = "Genetic Distance (%)", ylab= "Mean D statistic",cex =1,
               pch=16, font.lab=2,  xlim=c(0,4) )
-lm1<-lm(mean.d ~gen.dis)
+lm1<-lm(mean.d ~  gen.dis)
 summary(lm1)
-
 
